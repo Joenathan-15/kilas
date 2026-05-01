1532ea91c0cf194b4fa405ea5e048dcbf2fce015
Joenathan <hazendas10@gmail.com>
2026-05-01 19:38:26 +0700

    feat: define product model and implement initial database seeding logic

--- backend/model/product.go
+++ backend/model/product.go
@@ -3,13 +3,17 @@ package model
 import "time"
 
 type Product struct {
-	ID          uint      `gorm:"primaryKey" json:"id"`
-	Name        string    `gorm:"size:255;uniqueIndex;not null" json:"name"`
-	Price       int       `gorm:"not null" json:"price"`
-	Quantity    int       `gorm:"not null" json:"quantity"`
-	Type        string    `gorm:"size:50;not null;default:'currency'" json:"type"`
-	IsListed    bool      `gorm:"default:true" json:"is_listed"`
-	Description string    `gorm:"type:text" json:"description"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID            uint      `gorm:"primaryKey" json:"id"`
+	Name          string    `gorm:"size:255;uniqueIndex;not null" json:"name"`
+	NameID        string    `gorm:"size:255" json:"name_id"`
+	NameEN        string    `gorm:"size:255" json:"name_en"`
+	Price         int       `gorm:"not null" json:"price"`
+	Quantity      int       `gorm:"not null" json:"quantity"`
+	Type          string    `gorm:"size:50;not null;default:'currency'" json:"type"`
+	IsListed      bool      `gorm:"default:true" json:"is_listed"`
+	Description   string    `gorm:"type:text" json:"description"`
+	DescriptionID string    `gorm:"type:text" json:"description_id"`
+	DescriptionEN string    `gorm:"type:text" json:"description_en"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
 }
